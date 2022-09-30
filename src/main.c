59aa359fe4f3dc100c0c49a130af92b7c65dec87
Joachim Leroux <jleroux@c2r6s5.42lausanne.ch>
2022-09-30 19:02:59 +0200

    v1 of expander

--- src/main.c
+++ src/main.c
@@ -10,34 +10,6 @@ typedef struct	s_envp
 	char	*value[3];
 }				t_envp;
 
-t_envp		envp = {{"KEY1", "USER", "KEY2"}, {"VAL1", "jleroux", "VAL2"}};
-
-char	*find_var(const char *str)
-{
-	int	i;
-
-	i = -1;
-	while (envp.key[++i])
-	{
-		if (!ft_strncmp(str + 1, envp.key[i], ft_strlen(envp.key[i]) + 1))
-			return(envp.value[i]);
-	}
-	return (NULL);
-}
-
-char	*expand(const char *str)
-{
-	char	*var;
-
-	while (*str)
-	{
-		if (*str == '$' && *str + 1 != '?')
-			var = find_var(str);
-		str++;
-	}
-	return (var);
-}
-
 char	*rl_gets()
 {
 	static char	*line_read = (char *) NULL;
@@ -76,22 +48,113 @@ void	pipex(t_cmd *cmds)
 }
 */
 
-int	launch_minishell(char *cmdline)
+struct	s_cmd
 {
-	(void) cmdline;
+	char	*argv;
+	char	*input_name;
+	char	*output_name;
+	int		input_fd;
+	int		output_fd;
+}		t_cmd;
+
+//t_cmd	cmdlist[];
+
+char	*ft_strinsert(const char *str1, const char *str2, int start, int end)
+{
+	char	*new_str;
+
+	new_str = ft_substr(str1, 0, start - 2);
+	new_str = ft_strjoin(new_str, str2);
+	new_str = ft_strjoin(new_str, &str1[end]);
+	//ft_free(str1);
+	//ft_free(str2);
+	return (new_str);
+}
+
+void	expand(const char *str)
+{
+	int		i;
+	char	*var;
+	char	*new_str;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == '$' && str[i + 1] != '?')
+		{
+			var = ft_strtrim(str, "$");
+			printf("%s\n", var);
+			new_str = ft_strinsert(str, getenv(var), i, i + ft_strlen(var));
+			printf("%s\n", new_str);
+			ft_free(var);
+		}
+	}
+	return ;
+}
+
+char	**lex(char const *s)
+{
+	unsigned int	start;
+	unsigned int	end;
+	unsigned int	s_quote;
+	unsigned int	d_quote;
+	unsigned int	nb;
+	char			**list;
+
+	if (!s)
+		return (NULL);
+	start = 0;
+	nb = 0;
+	s_quote = 0;
+	d_quote = 0;
+	list = malloc(1024);//(1 + nb_words(s, c)) * sizeof(char *));
+	if (!list)
+		return (NULL);
+	while (s[start])
+	{
+		while (s[start] == ' ')
+			start++;
+		end = start;
+		while (s[end] && (s[end] != ' ' || s_quote || d_quote))
+		{
+			if (s[end] == '"' && !s_quote)
+				d_quote = (d_quote + 1) % 2;
+			else if (s[end] == '\'' && !d_quote)
+				s_quote = (s_quote + 1) % 2;
+			end++;
+		}
+		list[nb++] = ft_substr(s, start, end - start);
+		start = end;
+	}
+	list[nb] = NULL;
+	return (list);
+}
+
+int	launch_minishell(char *line)
+{
+	char	**tokens;
+
+	tokens = lex(line); //Warning: Check some syntax errors beforehand
+	for (int i = 0; tokens[i]; i++)
+		expand(tokens[i]);
+	for (int i = 0; tokens[i]; i++)
+		printf("%s\n", tokens[i]);
 	/*
-	tokens = lex(cmdline); //Warning: Check some syntax errors beforehand
+	(void) line;
 	expand(tokens); //Warning: Don't expand inside single quotes + $?
 	split_metachar(tokens); //Warning: Don't split >>, && and ||
 	cmds = parse(tokens); //Create a list of cmds w/ corresponding i/o
 	pipex(cmds); //Warning: Be sure to execute OUR built-ins + $?
+
+	pipex(parse(expand(lex(line))));
 	*/
 	return (0);
 }
 
+#define EXIT 1
+
 //Warnings!
 //Try to run w/o env: env -i ./minishell
-#define EXIT 1
 int	main(int ac, char **av, char **envp)
 {
 	char	*line;
@@ -100,17 +163,17 @@ int	main(int ac, char **av, char **envp)
 
 	(void) envp;
 	errno = 0;
-	signal = -1;
+	signal = 0;
 	//handle_signals();
 	if (ac >= 3 && !ft_strncmp(av[1], "-c", 3))
 		return (launch_minishell(av[2]));
 	while (signal != EXIT)
 	{
 		line = rl_gets();
-		//errno = launch_minishell(line);
-		printf("%s\n", line);
+		errno = launch_minishell(line);
+		//printf("%s\n", line);
 		free(line);
-		signal += 1;
+		signal ++;
 	}
 	return (errno);
 }

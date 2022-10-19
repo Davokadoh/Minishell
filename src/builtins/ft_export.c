c625a77ab85c1c6b11b49de5ba0f33737fd7788c
Blaise Tchimanga <btchiman@c2r9s5.42lausanne.ch>
2022-10-19 13:48:44 +0200

    export update

--- src/builtins/ft_export.c
+++ src/builtins/ft_export.c
@@ -12,8 +12,67 @@
 
 #include "../include/minishell.h"
 /*
+ * // ajoute a la liste de variable d'environnement
 int ft_export(char **args)
 {
     //t_envp  *env;
 
 }*/
+
+/*
+ * char	*get_variable_name(char *variable)
+{
+	int		index;
+	int		start;
+	int		end;
+	char	*name;
+
+	index = 0;
+	start = 0;
+	if (!variable)
+		return (NULL);
+	while (variable[index])
+	{
+		if (variable[index] == '=')
+		{
+			end = index - 1;
+			name = malloc_substrcpy(variable, start, end);
+			return (name);
+		}
+		index++;
+	}
+	end = index;
+	name = malloc_substrcpy(variable, start, end);
+	return (name);
+}
+
+ char	*get_env_variable_value(char *variable)
+{
+	int		i;
+	int		start;
+	int		end;
+	char	*value;
+
+	i = 0;
+	if (!variable)
+		return (NULL);
+	while (variable[i])
+	{
+		if (variable[i] == '=' && !(is_blank(variable[i + 1])))
+		{
+			start = i + 1;
+			end = ft_strlen(variable);
+			value = malloc_substrcpy(variable, start, end);
+			if (is_value_null(value))
+			{
+				free(value);
+				return (NULL);
+			}
+			return (value);
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+ */
